11ab2364988c84564bce93422e2115f8f3677989
Gerard Causse <gcausse@epri.com>
2026-03-04 08:22:46 -0500

    Add method to find by list

--- src/trello/boards.py
+++ src/trello/boards.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, select
 
-from trello.database import BoardRecord
+from trello.database import BoardRecord, ListRecord
 
 
 class BoardRepository:
@@ -9,5 +9,15 @@ class BoardRepository:
 
     def find(self, board_id: int) -> BoardRecord | None:
         statement = select(BoardRecord).where(BoardRecord.id == board_id).limit(1)
+
+        return self._db.exec(statement).first()
+
+    def find_by_list(self, list_id: int) -> BoardRecord | None:
+        statement = (
+            select(BoardRecord)
+            .join(ListRecord)
+            .where(ListRecord.id == list_id)
+            .limit(1)
+        )
 
         return self._db.exec(statement).first()
